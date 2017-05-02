8ded7828512328b8d3fb61afc0c25a3c15b9c7af
ezehurt <ezehurt@gmail.com>
2017-05-02 10:27:22 -0300

    added artist, comicBookArtist and role classes . Modified Contect modelbuilder to not pluralize tables.

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -8,12 +8,21 @@ namespace ComicBookGalleryModel.Models
 {
     public class ComicBook
     {
+
+        public ComicBook()
+        {
+            Artists = new List<ComicBookArtist>();
+        }
         public int Id { get; set; }
-        public Series Series { get; set; }
+        public int SeriesId { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public double? AverageRating { get; set; }
+
+
+        public Series Series { get; set; }
+        public ICollection<ComicBookArtist> Artists { get; set; }
 
 
         public string DisplayText
@@ -21,5 +30,14 @@ namespace ComicBookGalleryModel.Models
             {
                 return $"{Series?.Title} #{IssueNumber}";
             } }
+
+        public void AddArtist(Artist artist, Role role)
+        {
+            Artists.Add(new ComicBookArtist()
+            {
+                Artist = artist,
+                Role = role
+            });
+        }
     }
 }
